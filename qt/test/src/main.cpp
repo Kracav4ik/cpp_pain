2c14fe7a929b349fdbbb8a99e8cd8269d9736a67
Kracav4ik <dikama2013@yandex.ru>
2016-06-18 20:22:08 +0300

    хелловорлд на сконсе и кутэ (с кутэ!)

--- qt/test/src/main.cpp
+++ qt/test/src/main.cpp
@@ -1,6 +1,12 @@
-#include <stdio.h>
-
-int main() {
-    printf("hello world\n");
-    return 0;
+#include <QApplication>
+#include <QDialog>
+#include <QLabel>
+ 
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    QDialog *dialog = new QDialog;
+    QLabel *label = new QLabel(dialog);
+    label->setText("<font color=red>Hello, World!</font>");
+    dialog->show();
+    return app.exec();
 }
