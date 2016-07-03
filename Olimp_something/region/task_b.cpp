b2f04d0874d425c3b5665965dae4591ae8acdd7e
Kracav4ik <dikama@yandex.ru>
2016-07-04 00:19:13 +0300

    Задача b;

--- Olimp_something/region/task_b.cpp
+++ Olimp_something/region/task_b.cpp
@@ -3,34 +3,7 @@
 #include "utils.h"
 #include "text_file.h"
 
-template <int i>
-struct Number;
-
-template <>
-struct Number<1> {
-    enum {n = 0};
-};
-
-template <>
-struct Number<2> {
-    enum {n = 0};
-};
-
-template <int i>
-struct Number {
-    enum {n = Number<i-1>::n + Number<i-2>::n};
-};
-
-
-
-// template <int H, int W>
-// void 
-
 int main(){
-    print(Number<150000>::n);
-    return 0;
-    
-    
     TextFile file;
     if (!file.open("input.txt")){
         return 0;
@@ -43,17 +16,15 @@ int main(){
     LongInt b = seter[1].str();
     LongInt c = seter[2].str();
     
-    int h = a.as_int();
-    int w = b.as_int();
+    int w = a.as_int();
+    int h = b.as_int();
     int n = c.as_int();
     
     
-    char list[h][w];
-    
-    // 0.5 + &(list);
-    
-    for (int i = 0; i < h; i += 1) {
-        for (int j = 0; j < w; j += 1) {
+    char list[w][h];
+        
+    for (int i = 0; i < w; i += 1) {
+        for (int j = 0; j < h; j += 1) {
             list[i][j] = '.';
         }
     }
@@ -68,20 +39,20 @@ int main(){
         int one = LongInt(seter[0].str()).as_int() - 1;
         int two = LongInt(seter[1].str()).as_int() - 1;
         int three = LongInt(seter[2].str()).as_int() - 1;
-        int four = LongInt(seter[3].str()).as_int();
+        int four = LongInt(seter[3].str()).as_int() - 1;
         
-        for (int m = two; m < four; m += 1) {
-            list[m][two] = i + 'a';
-            list[m][four - 1] = i + 'a';
+        for (int m = two; m <= four; m += 1) {
+            list[one][m] = i + 'a';
+            list[three][m] = i + 'a';
         }
-        for (int v = one; v < three; v += 1) {
-            list[one][v] = i + 'a';
-            list[three][v] = i + 'a';
+        for (int v = one; v <= three; v += 1) {
+            list[v][two] = i + 'a';
+            list[v][four] = i + 'a';
         }
     }
     
-    for (int i = 0; i < h; i += 1) {
-        for (int j = 0; j < w; j += 1) {
+    for (int i = 0; i < w; i += 1) {
+        for (int j = 0; j < h; j += 1) {
             printf("%c",list[i][j]);
         }
         printf("\n");
